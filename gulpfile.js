dc31835e136706b06b553beb7de96e611ca101c0
pjhuang <506750334@qq.com>
2021-07-09 18:31:46 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,36 @@
 var gulp = require("gulp");
 var ts =  require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var fs = require('fs')
+
+// gulp.task("default", function () {
+//     return gulp.src(paths.pages)
+//     .pipe(gulp.dest("dist"));
+// });
+
+const parentPath = "ScriptTs";
 
 
+function build(cb){
+    //文件夹内所有文件名
+    let paths = fs.readdirSync(parentPath); // console.log('fs.readFileSync(path)',);
+    
+    for (let i = 0; i < paths.length; i++) {
+        const element = paths[i];
+        let fileName = parentPath + "/" + element;
+        if(!fs.statSync(fileName).isDirectory()){
+            //当前路径是文件
+            continue;
+        }
 
-gulp.task("default", function () {
-    // return tsProject.src()
-    //     .pipe(tsProject())
-    //     .js.pipe(gulp.dest("dist"));
-    return tsProject.src().pipe(tsProject()).js.pipe()
-});
+        if(fs.statSync(parentPath + "/tsconfig.json").isFile()){
+            //已有tsconfig.json
+        }
+
+        console.log('element',element);
+
+    }
+    cb();
+}
+
+exports.gulpBuild = build;
---
+++ js/sort.d.ts
@@ -0,0 +1,3 @@
+declare namespace Test {
+}
+//# sourceMappingURL=sort.d.ts.map
